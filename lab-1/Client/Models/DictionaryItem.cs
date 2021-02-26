bd53a0fa12586c792aae6994dae047dbc8a5a1e3
Ulyan Yunakh <ulyanyunakh@gmail.com>
2021-02-26 09:25:56 +0300

    add in DictionaryItem value by default

--- lab-1/Client/Models/DictionaryItem.cs
+++ lab-1/Client/Models/DictionaryItem.cs
@@ -11,16 +11,16 @@ namespace Client.Models
         public bool IsWordform { get; set; } // словоформа
         public int Frequency { get; set; } // частота встречаемости
 
-        public string PartOfSpeech { get; set; } // часть речи
-        public string Kind { get; set; } // род
-        public string Number { get; set; } // число
-        public string Case { get; set; } // падеж
+        public string PartOfSpeech { get; set; } = "Пусто";  // часть речи
+        public string Kind { get; set; } = "Пусто"; // род
+        public string Number { get; set; } = "Пусто"; // число
+        public string Case { get; set; } = "Пусто"; // падеж
         public bool IsShort { get; set; } // краткость
-        public string Person { get; set; } // лицо
-        public string Time { get; set; } // время
-        public string Form { get; set; } // вид
-        public string Degree { get; set; } // степень сравнения
-        public string Animality { get; set; } // одушевленность
+        public string Person { get; set; } = "Пусто"; // лицо
+        public string Time { get; set; } = "Пусто"; // время
+        public string Form { get; set; } = "Пусто"; // вид
+        public string Degree { get; set; } = "Пусто"; // степень сравнения
+        public string Animality { get; set; } = "Пусто"; // одушевленность
 
         public bool IsDescription { get; set; }
         public string Description { get; set; } // неформатированная информация
