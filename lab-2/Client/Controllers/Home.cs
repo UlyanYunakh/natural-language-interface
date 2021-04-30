494cd4930b0e62cbe21859fb83b06c1afdee391b
Ulyan Yunakh <ulyanyunakh@gmail.com>
2021-04-30 21:50:04 +0300

    complete index page

--- lab-2/Client/Controllers/Home.cs
+++ lab-2/Client/Controllers/Home.cs
@@ -3,23 +3,20 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using Client.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace Client.Controllers
 {
     public class Home : Controller
     {
-        // private readonly ILogger<Home> _logger;
-
-        public Home(ILogger<Home> logger)
-        {
-            // _logger = logger;
-        }
-
         [HttpGet]
         public IActionResult Index()
         {
@@ -28,7 +25,7 @@ namespace Client.Controllers
         }
 
         [HttpPost]
-        public IActionResult Index(IFormFile file)
+        public async Task<IActionResult> Index(IFormFile file)
         {
             if (file != null)
             {
@@ -37,15 +34,55 @@ namespace Client.Controllers
                 {
                     fileBytes = binaryReader.ReadBytes((int)file.Length);
                 }
-                string text = Encoding.UTF8.GetString(fileBytes);
+                ViewBag.Text = Encoding.UTF8.GetString(fileBytes);
 
-                ViewBag.Text = text;
-                ViewBag.IsResult = true;
+                ViewBag.ResponceModel = null;
+                ViewBag.ResponceModel = await PostToPythonServer(ViewBag.Text);
+
+                if (ViewBag.ResponceModel == null)
+                    ViewBag.ServerError = true;
+                else
+                    ViewBag.IsResult = true;
 
                 return View();
             }
-            ViewBag.IsResult = false;
-            return View();
+            else
+            {
+                ViewBag.IsResult = false;
+                return View();
+            }
+        }
+
+        private async Task<ResponceModel> PostToPythonServer(string text)
+        {
+            ResponceModel responceModel = null;
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri("http://localhost:8080/");
+                client.DefaultRequestHeaders
+                    .Accept
+                    .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "relativeAddress");
+
+                string jsonRequest = JsonConvert.SerializeObject(new RequestModel() { Text = text });
+                request.Content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+                await client.SendAsync(request)
+                    .ContinueWith(responseTask =>
+                    {
+                        byte[] responce = responseTask.Result.Content.ReadAsByteArrayAsync().Result;
+                        string json = Encoding.UTF8.GetString(responce);
+                        responceModel = JsonConvert.DeserializeObject<ResponceModel>(json);
+                    });
+
+                return responceModel;
+            }
+            catch
+            {
+                return null;
+            }
         }
 
         public IActionResult Help()
