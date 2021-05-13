734551a308ca77acae065611c4c6a451daaa0958
Ulyan Yunakh <ulyanyunakh@gmail.com>
2021-05-13 13:39:48 +0300

    dialog system

--- lab-4/Client/Data/empty.cs
+++
@@ -1,6 +0,0 @@
-using System.Threading.Tasks;
-
-namespace Client.Data
-{
-    
-}
--- lab-4/Client/Services/Dialog.cs
+++ lab-4/Client/Services/Dialog.cs
@@ -1,6 +1,12 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 using Client.Data;
+using Newtonsoft.Json;
 
 namespace Client.Services
 {
@@ -11,17 +17,91 @@ namespace Client.Services
         public DialogService()
         {
             _modules = new List<IResponce>();
-            _modules.Add(CreateModule(new HelloResponce()));
+            _modules.Add(CreateModule(
+                new BreakfastResponce(),
+                new List<string>()
+                {
+                    "завтрак",
+                    "утро"
+                }));
+            _modules.Add(CreateModule(
+                new LunchResponce(),
+                new List<string>()
+                {
+                    "обед",
+                    "середина"
+                }));
+            _modules.Add(CreateModule(
+                new DinnerResponce(),
+                new List<string>()
+                {
+                    "ужин",
+                    "вечер"
+                }));
+            _modules.Add(CreateModule(
+                new MealResponce(),
+                new List<string>()
+                {
+                    "кушать",
+                    "голод"
+                }));
+            _modules.Add(CreateModule(
+                new HelloResponce(),
+                new List<string>()
+                {
+                    "привет"
+                }));
         }
 
-        private IResponce CreateModule(IResponce module)
+        private IResponce CreateModule(IResponce module, List<string> KeyWords)
         {
-            module.KeyWords = new List<string>(){"Привет"};
+            List<string> keyWords = new List<string>();
+            foreach (string word in KeyWords)
+                keyWords = keyWords.Union(PostToPythonServer(word)).ToList();
+            module.KeyWords = keyWords;
             return module;
         }
 
+        private record RequestModel
+        {
+            public string Text { get; set; }
+        }
+
+        private List<string> PostToPythonServer(string text)
+        {
+            List<string> responceModel = new List<string>();
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri("http://localhost:8080/");
+                client.DefaultRequestHeaders
+                    .Accept
+                    .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "relativeAddress");
+
+                string jsonRequest = JsonConvert.SerializeObject(new RequestModel() { Text = text });
+                request.Content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+
+                var responce = client.Send(request);
+
+                byte[] responceByte = responce.Content.ReadAsByteArrayAsync().Result;
+                string json = Encoding.UTF8.GetString(responceByte);
+                responceModel = JsonConvert.DeserializeObject<List<string>>(json);
+
+                return responceModel;
+            }
+            catch
+            {
+                return responceModel;
+            }
+        }
+
+
         public async Task<string> GetResponce(string request)
         {
+            request = request.ToLower();
             foreach (IResponce module in _modules)
             {
                 string responce = await Task.FromResult(module.GetResponce(request));
